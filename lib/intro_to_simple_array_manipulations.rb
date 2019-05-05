6f89aa58fa56f2748efdafe8d1fc3b6bc591b24f
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-05 17:40:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,6 +19,10 @@ def using_shift(array)
   return element
 end
 
+def shift_with_args(array)
+  array.shift(2)
+end
+
 def using_concat(first_array, second_array)
   first_array.concat(second_array)
 end
